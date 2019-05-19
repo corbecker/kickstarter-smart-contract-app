06ac2d0dd5f011bf8940e586465fda9354b3bc8e
corbecker <corbinbecker@protonmail.com>
2019-05-19 08:59:26 +0100

    approvers & minimum contribution tests

---
+++ test/Campaign.test.js
@@ -0,0 +1,56 @@
+const Web3 = require('web3');
+const ganache = require('ganache-cli');
+const assert = require('assert');
+const compiledFactory = require('../ethereum/build/CampaignFactory.json');
+const compiledCampaign = require('../ethereum/build/Campaign.json');
+
+const provider = ganache.provider();
+const web3 = new Web3(provider);
+
+let accounts;
+let factory;
+let campaignAddress;
+let campaign;
+
+beforeEach(async () => {
+  accounts = await web3.eth.getAccounts();
+  factory = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
+    .deploy({ data: compiledFactory.bytecode })
+    .send({ from: accounts[0], gas: '1000000' });
+
+  await factory.methods.createCampaign('1000').send({from: accounts[0], gas: '1000000'});
+
+  [campaignAddress] = await factory.methods.getDeployedCampaigns().call();
+
+  campaign = await new web3.eth.Contract(JSON.parse(compiledCampaign.interface), campaignAddress);
+});
+
+describe('Campaigns', () => {
+  it('deploys a factory and a campaign', () => {
+    assert.ok(factory.options.address);
+    assert.ok(campaign.options.address);
+  });
+  it('sets campaign owner to manager', async () => {
+    const manager = await campaign.methods.manager().call();
+    assert.equal(accounts[0], manager);
+  });
+  it('allows contributions & marks as approvers', async () => {
+    await campaign.methods.contribute().send({
+      from: accounts[1],
+      value: '1000'
+    });
+    const isApprover = await campaign.methods.approvers(accounts[1]).call();
+    assert.equal(true, isApprover);
+  });
+  it('requires a minimum contribution', async() => {
+    try{
+      await campaign.methods.contribute().send({
+        from: accounts[1],
+        value: '100'
+      });
+      assert(false);
+    }catch(err) {
+      assert(err);
+    }
+  });
+});
