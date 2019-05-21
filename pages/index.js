31fa8a15dbefda12f0c34526493d5af0e3dcfa78
corbecker <corbinbecker@protonmail.com>
2019-05-21 10:46:55 +0100

    refactor to getInitialProps

--- pages/index.js
+++ pages/index.js
@@ -2,18 +2,20 @@ import React, { Component } from 'react';
 import instance from '../ethereum/factory';
 
 class App extends Component{
+
+  static async getInitialProps() {
+    const campaigns = await instance.methods.getDeployedCampaigns().call();
+    return {campaigns};
+  }
+
   constructor(props) {
     super(props);
 
     this.state = {
-      campaigns: []
+      campaigns: props.campaigns
     }
   }
 
-  async componentDidMount() {
-    const campaigns = await instance.methods.getDeployedCampaigns().call();
-    this.setState({campaigns});
-  }
   render() {
     return (
       <h1>{this.state.campaigns}</h1>
